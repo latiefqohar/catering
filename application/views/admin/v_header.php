e86dbbc9803d8720fa70609eb7cab5c44f9953c7
latiefqohar <latiefqohar97@gmail.com>
2020-01-31 22:40:18 +0700

    add sidebar

--- application/views/admin/v_header.php
+++ application/views/admin/v_header.php
@@ -169,93 +169,108 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item has-treeview menu-open">
+          <li class="nav-item has-treeview ">
             <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <i class="nav-icon fas fa-copy"></i>
               <p>
-                Dashboard
+                Master Data
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="./index.html" class="nav-link">
+                <a href="<?= base_url('admin/Perusahaan'); ?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v1</p>
+                  <p>Data Perusahaan</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="./index2.html" class="nav-link active">
+                <a href="<?= base_url('admin/Menu'); ?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v2</p>
+                  <p>Data Menu</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="./index3.html" class="nav-link">
+                <a href="<?= base_url('admin/Kupon'); ?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v3</p>
+                  <p>Data Kupon</p>
                 </a>
               </li>
             </ul>
-          </li>
-          <li class="nav-item">
-            <a href="pages/widgets.html" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Widgets
-                <span class="right badge badge-danger">New</span>
-              </p>
-            </a>
           </li>
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
               <p>
-                Layout Options
+                Transaksi
                 <i class="fas fa-angle-left right"></i>
-                <span class="badge badge-info right">6</span>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="pages/layout/top-nav.html" class="nav-link">
+                <a href="<?= base_url('admin/Transaksi'); ?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Top Navigation</p>
+                  <p>Data Transaksi</p>
+                  <span class="badge badge-info right">6</span>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="pages/layout/boxed.html" class="nav-link">
+                <a href="<?= base_url('admin/Transaksi/verifikasiPerorang'); ?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Boxed</p>
+                  <p> Menunggu Verifikasi (Perorang)</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="pages/layout/fixed-sidebar.html" class="nav-link">
+                <a href="<?= base_url('admin/Transaksi/verifikasiPerusahaan'); ?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Fixed Sidebar</p>
+                  <p>Menunggu (Verifikasi Perusahaan)</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="pages/layout/fixed-topnav.html" class="nav-link">
+                <a href="<?= base_url('admin/Transaksi/proses'); ?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Fixed Navbar</p>
+                  <p>Menunggu diproses</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="pages/layout/fixed-footer.html" class="nav-link">
+                <a href="<?= base_url('admin/Transaksi/kirim'); ?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Fixed Footer</p>
+                  <p>Menunggu dikirim</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="pages/layout/collapsed-sidebar.html" class="nav-link">
+                <a href="<?= base_url('admin/Transaksi/selesai'); ?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Collapsed Sidebar</p>
+                  <p>Selesai</p>
                 </a>
               </li>
             </ul>
           </li>
           <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+            <i class="fas fa-money-check-alt"></i>
+              <p>
+                Invoice
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="<?= base_url('admin/Tagihan'); ?>" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Tagihan</p>
+                  <span class="badge badge-info right">6</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="<?= base_url('admin/Invoice'); ?>" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p> Invoice</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <!-- <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-chart-pie"></i>
               <p>
@@ -680,7 +695,7 @@
               <i class="nav-icon far fa-circle text-info"></i>
               <p>Informational</p>
             </a>
-          </li>
+          </li> -->
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
---
+++ application/views/registration/ViewEditRegistration.php
@@ -0,0 +1,33 @@
+<div class="container mb-2">
+    <div class="card">
+        <div class="card-header">
+            Edit Data User
+        </div>
+        <div class="card-body">
+            <form action="<?= base_url(); ?>Registration/actionedit" method="post" enctype="multipart/form-data">
+                <input type="hidden" name="id" value="<?= $registration['id']; ?>">
+                <table class="table table-bordered ">
+                    <tr>
+                        <td>Nama</td>
+                        <td><input type="text" class="form-control" name="nama" value="<?= $registration['nama']; ?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Nama ilmiah</td>
+                        <td><input type="text" class="form-control" name="username"
+                                value="<?= $registration['username']; ?>"></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td>
+                            <input type="password" class="form-control" name="password">
+                            <small class="text-danger">isi password baru jika ingin merubah password</small>
+                        </td>
+                    </tr>
+
+                </table>
+                <button class="btn btn-primary float-right"><i class="fas fa-save"></i> Save </button>
+            </form>
+        </div>
+    </div>
+</div>
