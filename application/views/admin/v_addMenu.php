6342ab68ff195ae507b45eb54fde560facdb8814
latiefqohar <latiefqohar97@gmail.com>
2020-01-31 14:12:25 +0700

    add master menu

---
+++ application/views/admin/v_addMenu.php
@@ -0,0 +1,42 @@
+<div class="row">
+	<div class="col-md-12">
+		<div class="card card-primary">
+			<div class="card-header">
+				<h3 class="card-title">Tambah Data Menu</h3>
+			</div>
+			<!-- /.card-header -->
+			<!-- form start -->
+			<form role="form" action="<?= base_url('admin/Menu/actionAdd'); ?>" method="POST" enctype="multipart/form-data">
+				<div class="card-body">
+					<div class="form-group">
+						<label for="nama">Nama Menu</label>
+						<input type="text" class="form-control" id="nama" name="nama"
+							placeholder="Masukkan Nama Menu" required>
+					</div>
+					<div class="form-group">
+						<label for="deskripsi">Deskripsi</label>
+						<input type="text" class="form-control" id="deskripsi" name="deskripsi" placeholder="Masukkan deskripsi" required>
+                    </div>
+                    <div class="form-group">
+						<label for="harga">Harga</label>
+						<input type="number" class="form-control" id="harga" name="harga" placeholder="Rp." required>
+                    </div>
+                    <div class="form-group">
+                    <label for="exampleInputFile">Gambar</label>
+                    <div class="input-group">
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" name="foto" id="exampleInputFile">
+                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                      </div>
+                    </div>
+                  </div>
+				</div>
+				<!-- /.card-body -->
+				<div class="card-footer">
+					<button type="submit" class="btn btn-primary">Simpan</button>
+				</div>
+			</form>
+		</div>
+		<!-- /.card -->
+	</div>
+</div>
