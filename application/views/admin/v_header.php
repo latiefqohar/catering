7291e20de4152f7b8ff8ff73460c6885091a417e
latiefqohar <latiefqohar97@gmail.com>
2020-02-13 10:26:59 +0700

    revisi UI dikit heheh

--- application/views/admin/v_header.php
+++ application/views/admin/v_header.php
@@ -1,37 +1,38 @@
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <title>Catering</title>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+	<title>Catering</title>
 	<link rel="icon" type="image/png" href="<?= base_url('assets/catering.png'); ?>">
 
-  <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="<?= base_url(); ?>assets/admin/plugins/fontawesome-free/css/all.min.css">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="<?= base_url(); ?>assets/admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="<?= base_url(); ?>assets/admin/dist/css/adminlte.min.css">
-  <!-- Google Font: Source Sans Pro -->
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-  <!-- DataTables -->
-  <link rel="stylesheet" href="<?= base_url(); ?>assets/admin/plugins/datatables-bs4/css/dataTables.bootstrap4.css">
+	<!-- Font Awesome Icons -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/admin/plugins/fontawesome-free/css/all.min.css">
+	<!-- overlayScrollbars -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+	<!-- Theme style -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/admin/dist/css/adminlte.min.css">
+	<!-- Google Font: Source Sans Pro -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+	<!-- DataTables -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/admin/plugins/datatables-bs4/css/dataTables.bootstrap4.css">
 </head>
-<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
-<div class="wrapper">
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
-      </li>
-    </ul>
 
-    <!-- SEARCH FORM
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+	<div class="wrapper">
+		<!-- Navbar -->
+		<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+			<!-- Left navbar links -->
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+				</li>
+			</ul>
+
+			<!-- SEARCH FORM
     <form class="form-inline ml-3">
       <div class="input-group input-group-sm">
         <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
@@ -43,166 +44,176 @@
       </div>
     </form> -->
 
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-      <!-- Messages Dropdown Menu -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-user"></i>
-          <?= $this->session->userdata('user'); ?> 
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <a href="<?= base_url('Auth/logout'); ?>" class="dropdown-item">
-           Logout
-          </a>
-      </li>
-    </ul>
-  </nav>
-  <!-- /.navbar -->
+			<!-- Right navbar links -->
+			<ul class="navbar-nav ml-auto">
+				<!-- Messages Dropdown Menu -->
+				<li class="nav-item dropdown">
+					<a class="nav-link" data-toggle="dropdown" href="#">
+						<i class="far fa-user"></i>
+						<?= $this->session->userdata('user'); ?>
+					</a>
+					<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+						<a href="<?= base_url('Auth/logout'); ?>" class="dropdown-item">
+							Logout
+						</a>
+				</li>
+			</ul>
+		</nav>
+		<!-- /.navbar -->
 
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="<?= base_url(); ?>assets/admin/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light">Catering</span>
-    </a>
+		<!-- Main Sidebar Container -->
+		<aside class="main-sidebar sidebar-dark-primary elevation-4">
+			<!-- Brand Logo -->
+			<a href="<?= base_url('admin/Dashboard'); ?>" class="brand-link">
+				<img src="<?= base_url(); ?>assets/catering.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+					style="opacity: .8">
+				<span class="brand-text font-weight-light">Catering</span>
+			</a>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="<?= base_url(); ?>assets/admin/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
-        </div>
-      </div>
+			<!-- Sidebar -->
+			<div class="sidebar">
+				<!-- Sidebar user panel (optional) -->
+				<div class="user-panel mt-3 pb-3 mb-3 d-flex">
+					<div class="image">
+						<img src="<?= base_url(); ?>assets/admin/dist/img/user2-160x160.jpg" class="img-circle elevation-2"
+							alt="User Image">
+					</div>
+					<div class="info">
+						<a href="#" class="d-block"><?= $this->session->userdata('user'); ?> </a>
+					</div>
+				</div>
 
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
+				<!-- Sidebar Menu -->
+				<nav class="mt-2">
+					<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+						<!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item has-treeview ">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-copy"></i>
-              <p>
-                Master Data
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="<?= base_url('admin/Perusahaan'); ?>" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Data Perusahaan</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?= base_url('admin/Menu'); ?>" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Data Menu</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?= base_url('admin/Kupon'); ?>" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Data Kupon</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-bars"></i>
-              <p>
-                Transaksi
-                <i class="fas fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="<?= base_url('admin/Transaksi'); ?>" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Data Transaksi</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?= base_url('admin/Transaksi/verifikasiPerorang'); ?>" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Verifikasi (Perorang)</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?= base_url('admin/Transaksi/verifikasiPerusahaan'); ?>" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Verifikasi (Perusahaan)</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?= base_url('admin/Transaksi/proses'); ?>" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Menunggu diproses</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?= base_url('admin/Transaksi/kirim'); ?>" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Menunggu dikirim</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?= base_url('admin/Transaksi/selesai'); ?>" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Selesai</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-            <i class="fas fa-money-check-alt"></i>  
-              <p>
-                    Invoice
-                <i class="fas fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="<?= base_url('admin/Tagihan'); ?>" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Tagihan</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?= base_url('admin/Invoice'); ?>" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p> Invoice</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-            <i class="fas fa-users"></i>  
-              <p>
-                    Registration
-                <i class="fas fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="<?= base_url('Registration'); ?>" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>User</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <!-- <li class="nav-item has-treeview">
+						<li class="nav-item ">
+							<a href="<?= base_url('admin/Dashboard'); ?>" class="nav-link">
+								<i class="nav-icon fas fa-tachometer-alt"></i>
+								<p>
+									Dashboard
+									<i class="right fas fa-angle-left"></i>
+								</p>
+							</a>
+						</li>
+						<li class="nav-item has-treeview ">
+							<a href="#" class="nav-link">
+								<i class="nav-icon fas fa-database"></i>
+								<p>
+									Master Data
+									<i class="right fas fa-angle-left"></i>
+								</p>
+							</a>
+							<ul class="nav nav-treeview">
+								<li class="nav-item">
+									<a href="<?= base_url('admin/Perusahaan'); ?>" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Data Perusahaan</p>
+									</a>
+								</li>
+								<li class="nav-item">
+									<a href="<?= base_url('admin/Menu'); ?>" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Data Menu</p>
+									</a>
+								</li>
+								<li class="nav-item">
+									<a href="<?= base_url('admin/Kupon'); ?>" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Data Kupon</p>
+									</a>
+								</li>
+							</ul>
+						</li>
+						<li class="nav-item has-treeview">
+							<a href="#" class="nav-link">
+								<i class="nav-icon fas fa-bars"></i>
+								<p>
+									Transaksi
+									<i class="fas fa-angle-left right"></i>
+								</p>
+							</a>
+							<ul class="nav nav-treeview">
+								<li class="nav-item">
+									<a href="<?= base_url('admin/Transaksi'); ?>" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Data Transaksi</p>
+									</a>
+								</li>
+								<li class="nav-item">
+									<a href="<?= base_url('admin/Transaksi/verifikasiPerorang'); ?>" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Verifikasi (Perorang)</p>
+									</a>
+								</li>
+								<li class="nav-item">
+									<a href="<?= base_url('admin/Transaksi/verifikasiPerusahaan'); ?>" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Verifikasi (Perusahaan)</p>
+									</a>
+								</li>
+								<li class="nav-item">
+									<a href="<?= base_url('admin/Transaksi/proses'); ?>" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Menunggu diproses</p>
+									</a>
+								</li>
+								<li class="nav-item">
+									<a href="<?= base_url('admin/Transaksi/kirim'); ?>" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Menunggu dikirim</p>
+									</a>
+								</li>
+								<li class="nav-item">
+									<a href="<?= base_url('admin/Transaksi/selesai'); ?>" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Selesai</p>
+									</a>
+								</li>
+							</ul>
+						</li>
+						<li class="nav-item has-treeview">
+							<a href="#" class="nav-link">
+								<i class="nav-icon fas fa-money-check-alt"></i>
+								<p>
+									Invoice
+									<i class="fas fa-angle-left right"></i>
+								</p>
+							</a>
+							<ul class="nav nav-treeview">
+								<li class="nav-item">
+									<a href="<?= base_url('admin/Tagihan'); ?>" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Tagihan</p>
+									</a>
+								</li>
+								<li class="nav-item">
+									<a href="<?= base_url('admin/Invoice'); ?>" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p> Invoice</p>
+									</a>
+								</li>
+							</ul>
+						</li>
+						<li class="nav-item has-treeview">
+							<a href="#" class="nav-link">
+								<i class="nav-icon  fas fa-users"></i>
+								<p>
+									Registration
+									<i class="fas fa-angle-left right"></i>
+								</p>
+							</a>
+							<ul class="nav nav-treeview">
+								<li class="nav-item">
+									<a href="<?= base_url('Registration'); ?>" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>User</p>
+									</a>
+								</li>
+							</ul>
+						</li>
+						<!-- <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-chart-pie"></i>
               <p>
@@ -628,31 +639,31 @@
               <p>Informational</p>
             </a>
           </li> -->
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
+					</ul>
+				</nav>
+				<!-- /.sidebar-menu -->
+			</div>
+			<!-- /.sidebar -->
+		</aside>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <!-- <h1 class="m-0 text-dark">Dashboard v2</h1> -->
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <!-- <li class="breadcrumb-item"><a href="#">Home</a></li>
+		<!-- Content Wrapper. Contains page content -->
+		<div class="content-wrapper">
+			<!-- Content Header (Page header) -->
+			<div class="content-header">
+				<div class="container-fluid">
+					<div class="row mb-2">
+						<div class="col-sm-6">
+							<!-- <h1 class="m-0 text-dark">Dashboard v2</h1> -->
+						</div><!-- /.col -->
+						<div class="col-sm-6">
+							<ol class="breadcrumb float-sm-right">
+								<!-- <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item active">Dashboard v2</li> -->
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-    <section class="content">
-      <div class="container-fluid">
+							</ol>
+						</div><!-- /.col -->
+					</div><!-- /.row -->
+				</div><!-- /.container-fluid -->
+			</div>
+			<!-- /.content-header -->
+			<section class="content">
+				<div class="container-fluid">
